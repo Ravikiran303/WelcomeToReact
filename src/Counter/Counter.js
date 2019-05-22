e4b76d606c4e14acbc1eb4ce8ed5cc99e7ed1e93
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-22 10:46:25 +0530

    add Increment,decrement functions

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -7,9 +7,34 @@ export class Counter extends Component {
     this.state = {
       counter: 0
     };
+    this.Increment = () => {
+      this.setState({
+        counter: this.state.counter + 1
+      });
+    };
+    this.Decrement = () => {
+      this.setState({
+        counter: this.state.counter - 1
+      });
+    };
+  }
+  componentDidMount() {
+    console.log("Component did mount");
+    console.log("----------------------");
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Component did Update");
+    console.log("----------------------");
   }
   render() {
-    return <div>Counter = {0}</div>;
+    console.log("Render");
+    return (
+      <div>
+        <button onClick={this.Increment}>Increment</button>
+        <button onClick={this.Decrement}>Decrement</button>
+        Counter = {this.state.counter}
+      </div>
+    );
   }
 }
 
